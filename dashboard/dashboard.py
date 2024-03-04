56c06618b5028cf16ad2458b7103cf35bd4a02cd
mlvmalvinn <152082129+mlvmalvinn@users.noreply.github.com>
2024-03-04 21:17:47 +0700

    Add files via upload

---
+++ dashboard/dashboard.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# Set style seaborn
+sns.set(style='darkgrid')
+
+# Menyiapkan data day_df
+day_df = pd.read_csv("day.csv")
+day_df.head()
+
+# Menghapus kolom tidak perlu
+drop_col = ['instant', 'temp', 'atemp', 'hum', 'windspeed']
+for i in day_df.columns:
+  if i in drop_col:
+    day_df.drop(labels=i, axis=1, inplace=True)
+
+# Konversi angka menjadi keterangan
+day_df['yr'] = day_df['yr'].map({
+    0: '2011', 1: '2012'
+})
+day_df['mnth'] = day_df['mnth'].map({
+    1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'Mei', 6: 'Jun',
+    7: 'Jul', 8: 'Ags', 9: 'Sep', 10: 'Okt', 11: 'Nov', 12: 'Des'
+})
+day_df['season'] = day_df['season'].map({
+    1: 'Musim Semi', 2: 'Musim Panas', 3: 'Musim Gugur', 4: 'Musim Dingin'
+})
+day_df['weekday'] = day_df['weekday'].map({
+    0: 'Minggu', 1: 'Senin', 2: 'Selasa', 3: 'Rabu', 4: 'Kamis', 5: 'Jumat', 6: 'Sabtu'
+})
+day_df['weathersit'] = day_df['weathersit'].map({
+    1: 'Cerah',
+    2: 'Berawan',
+    3: 'Hujan/Salju Ringan',
+    4: 'Cuaca Buruk'
+})
+    
+# Season_rent_df
+def create_season_rent_df(df):
+    season_rent_df = df.groupby(by='season')[['registered', 'casual']].sum().reset_index()
+    return season_rent_df
+
+# Monthly_rent_df
+def create_monthly_rent_df(df):
+    monthly_rent_df = df.groupby(by='mnth').agg({
+        'cnt': 'sum'
+    })
+    ordered_months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ]
+    monthly_rent_df = monthly_rent_df.reindex(ordered_months, fill_value=0)
+    return monthly_rent_df
+
+# Weather_rent_df
+def create_weather_rent_df(df):
+    weather_rent_df = df.groupby(by='weathersit').agg({
+        'cnt': 'sum'
+    })
+    return weather_rent_df
+
+
+# komponen filter
+min_date = pd.to_datetime(day_df['dteday']).dt.date.min()
+max_date = pd.to_datetime(day_df['dteday']).dt.date.max()
+ 
+with st.sidebar:
+    
+    # start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value= min_date,
+        max_value= max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = day_df[(day_df['dteday'] >= str(start_date)) & 
+                (day_df['dteday'] <= str(end_date))]
+
+# dataframe
+season_rent_df = create_season_rent_df(main_df)
+monthly_rent_df = create_monthly_rent_df(main_df)
+weather_rent_df = create_weather_rent_df(main_df)
+
+
+
+# judul
+st.header('mlvmalvinn Bicycle Rental')
+
+# jumlah penyewaan bulanan
+st.subheader('Rental perbulan')
+fig, ax = plt.subplots(figsize=(24, 8))
+ax.plot(
+    monthly_rent_df.index,
+    monthly_rent_df['cnt'],
+    marker='o', 
+    linewidth=2,
+    color='tab:green'
+)
+
+for index, row in enumerate(monthly_rent_df['cnt']):
+    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
+ax.tick_params(axis='x', labelsize=25, rotation=45)
+ax.tick_params(axis='y', labelsize=20)
+st.pyplot(fig)
+
+# jumlah penyewaan berdasarkan musim
+st.subheader('Rental Permusim')
+fig, ax = plt.subplots(figsize=(16, 8))
+
+sns.barplot(
+    x='season',
+    y='registered',
+    data=season_rent_df,
+    label='Registered',
+    color='tab:blue',
+    ax=ax
+)
+
+sns.barplot(
+    x='season',
+    y='casual',
+    data=season_rent_df,
+    label='Casual',
+    color='tab:orange',
+    ax=ax
+)
+
+for index, row in season_rent_df.iterrows():
+    ax.text(index, row['registered'], str(row['registered']), ha='center', va='bottom', fontsize=12)
+    ax.text(index, row['casual'], str(row['casual']), ha='center', va='bottom', fontsize=12)
+
+ax.set_xlabel(None)
+ax.set_ylabel(None)
+ax.tick_params(axis='x', labelsize=20, rotation=0)
+ax.tick_params(axis='y', labelsize=15)
+ax.legend()
+st.pyplot(fig)
+
+# Jumlah penyewaan berdasarkan cuaca
+st.subheader('Rental Dalam Cuaca')
+fig, ax = plt.subplots(figsize=(16, 8))
+colors=["tab:blue", "tab:orange", "tab:green"]
+
+sns.barplot(
+    x=weather_rent_df.index,
+    y=weather_rent_df['cnt'],
+    palette=colors,
+    ax=ax
+)
+
+for index, row in enumerate(weather_rent_df['cnt']):
+    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
+ax.set_xlabel(None)
+ax.set_ylabel(None)
+ax.tick_params(axis='x', labelsize=20)
+ax.tick_params(axis='y', labelsize=15)
+st.pyplot(fig)
+
+st.caption('Copyright (c) mlv_malvinn Simple Webs 2024')
